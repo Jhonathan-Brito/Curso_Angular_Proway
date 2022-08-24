1464a4a164d73f99abf8cffee82dc1ba5409a64a
jhonathan silva <jhonathan.silva@interne.com.br>
2022-08-24 14:52:38 -0300

    angular

---
+++ Fundamentos_angular/src/app/segundo-componente/segundo-componente.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-componente',
+  templateUrl: './segundo-componente.component.html',
+  styleUrls: ['./segundo-componente.component.css']
+})
+export class SegundoComponenteComponent {
+
+  nome = "João";
+  dataNascimento = "1990-05-01";
+  urlImagem = "/assets/versão.png";
+
+  mostrarDataNascimento() {
+    alert( `A data de nascimento do João é: ${this.dataNascimento}`)
+  }  
+
+
+
+}
